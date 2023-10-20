750ed21a4817f6d3a4396cc363adadae54465c50
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-10-20 16:32:57 +0700

    up

---
+++ Lab01WebApp/Controllers/SubjectController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab01Lib;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace Lab01WebApp.Controllers
+{
+    public class SubjectController : Controller
+    {
+        private HttpClient _httpClient = new HttpClient();
+        private string url = "https://localhost:7164/api/Subject";
+        public IActionResult Index()
+        {
+            var model=JsonConvert.DeserializeObject<IEnumerable<Subject>>(
+                                    _httpClient.GetStringAsync(url).Result);
+            return View(model);
+        }
+    }
+}
