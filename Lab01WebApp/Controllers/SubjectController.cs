437990c4b38877d2ccf6b5eb8ec7782ea4cb9e4f
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-10-20 17:26:54 +0700

    up

--- Lab01WebApp/Controllers/SubjectController.cs
+++ Lab01WebApp/Controllers/SubjectController.cs
@@ -15,5 +15,52 @@ namespace Lab01WebApp.Controllers
                                     _httpClient.GetStringAsync(url).Result);
             return View(model);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Subject subject) 
+        {
+            try
+            {
+                var model=_httpClient.PostAsJsonAsync<Subject>(url, subject).Result;
+                if (model.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index", "Subject");
+                }
+                else 
+                {
+                    ViewBag.Subject = "Failed to add subject";
+                }
+            }
+            catch (Exception ex) 
+            {
+                ViewBag.Subject = ex.Message;
+            }
+            return View();
+        }
+        public IActionResult Delete(string code)
+        {
+            try
+            {
+                var model = _httpClient.DeleteAsync(url + "/" + code).Result;
+                if (model.IsSuccessStatusCode)
+                {
+
+                    return RedirectToAction("Index", "Subject");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Fail");
+                }
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError(string.Empty, e.Message);
+
+            }
+            return View();
+        }
     }
 }
