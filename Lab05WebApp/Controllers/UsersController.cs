66cae63f63bc6498567c8375db19ac93bcde1b26
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-10-30 16:45:46 +0700

    up lab05

---
+++ Lab05WebApp/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab05WebApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
