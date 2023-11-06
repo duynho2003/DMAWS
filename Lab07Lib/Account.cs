b93260ee0babd0dbadcaf05054e0a3d5ac06dee1
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-11-06 14:51:14 +0700

    up

---
+++ Lab07Lib/Account.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab07Lib
+{
+    [Table("tbAccount")]
+    public class Account
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string? no { get; set; }
+        public int pincode { get; set; }
+    }
+}
