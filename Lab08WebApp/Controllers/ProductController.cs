53af9753bc2e30ec9e092aa4a1bf488f3ed9f91a
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-11-08 17:27:34 +0700

    up lab08

---
+++ Lab08WebApp/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab08WebApp.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+namespace Lab08WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private HttpClient _httpClient=new HttpClient();
+        private string urlc = "https://localhost:7102/api/Categories/";
+        private string urlp = "https://localhost:7102/api/Products/";
+        public IActionResult Index(int? cateId)
+        {
+            var model = JsonConvert.DeserializeObject<IEnumerable<Product>>(
+                                    _httpClient.GetStringAsync(urlp).Result);
+            if (cateId == null)
+            {
+                return View(model);
+            }
+            else 
+            {
+                model = model!.Where(p=>p.CategoryId.Equals(cateId)).ToList();
+                return View(model);
+            }
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab08WebApp/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab08WebApp.Models;
+
+public partial class Product
+{
+    public int ProductId { get; set; }
+
+    public string? ProductName { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public decimal? Price { get; set; }
+
+    public int? Quantity { get; set; }
+
+    public virtual Category? Category { get; set; }
+}
