7ad4a84508065c3f5bb72b5d7d747218fb0dae4d
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-11-10 14:24:00 +0700

    up lab09

---
+++ Lab09Lib/Shipper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09Lib
+{
+    [Table("Shippers")]
+    public class Shipper
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id {  get; set; }
+        public string? name {  get; set; }
+        public string? phone {  get; set; }
+        public string? photo {  get; set; }
+    }
+}
---
+++ Lab09Lib/Users.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab09Lib
+{
+    [Table("tbUsers")]
+    public class Users
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string? username { get; set; }
+        public string? password { get; set; }
+        public string? role { get; set; }
+    }
+}
