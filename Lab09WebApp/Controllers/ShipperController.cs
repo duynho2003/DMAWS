f117da95bd0451d3c2bdc88f705d94a91b2372fc
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-11-11 21:52:35 +0700

    up lab09 webapp delete not working

---
+++ Lab09WebApp/Controllers/ShipperController.cs
@@ -0,0 +1,83 @@
+﻿using Lab09Lib;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Lab09WebApp.Controllers
+{
+    public class ShipperController : Controller
+    {
+        private string url = "https://localhost:7045/api/Shippers/";
+        private HttpClient httpClient = new HttpClient();
+        public IActionResult Index()
+        {
+            var model = JsonConvert.DeserializeObject<IEnumerable<Shipper>>(
+                                        httpClient.GetStringAsync(url).Result);
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Shipper shipper, IFormFile file)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (file != null)
+                    {
+                        string path = Path.Combine("wwwroot/images", file.FileName);
+                        var stream = new FileStream(path, FileMode.Create);
+                        file.CopyToAsync(stream);
+                        shipper.photo = "/images/" + file.FileName;
+
+                        var model = httpClient.PostAsJsonAsync(url, shipper).Result;
+                        if (model.IsSuccessStatusCode)
+                        {
+                            return RedirectToAction("Index", "Shipper");
+                        }
+                        else
+                        {
+                            ViewBag.msg = "Fail...";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.msg = ex.Message;
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var model = httpClient.DeleteAsync(url + id).Result;
+                if (model.IsSuccessStatusCode)
+                {
+                    ViewBag.msg = "Delete shipper completed...";
+                }
+                else
+                {
+                    ViewBag.msg = "Delete failed!";
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.msg = ex.Message;
+            }
+            return View();
+        }
+    }
+}
---
+++ Lab09WebApp/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using Lab09Lib;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Lab09WebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private string url = "https://localhost:7045/api/Users/";
+        private HttpClient httpClient = new HttpClient();
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("uname");
+            return RedirectToAction("Login");
+        }
+        [HttpPost]
+        public IActionResult Login(string uname, string upass)
+        {
+            try
+            {
+                var user = JsonConvert.DeserializeObject<Users>(
+                                        httpClient.GetStringAsync(url+uname+"/"+upass).Result);
+                if (user != null)
+                {
+                    //session
+                    HttpContext.Session.SetString("uname", uname);
+                    if (user.role!.Equals("Admin"))
+                    {
+                        return RedirectToAction("Delete", "Shipper");
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Shipper");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Login fail...");
+                }
+                return View();
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError(string.Empty, "Login fail...");
+            }
+            return View();
+        }
+    }
+}
