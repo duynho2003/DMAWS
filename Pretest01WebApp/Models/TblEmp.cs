9593d42f1a6910348d45cb67de51107436229383
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-11-14 20:37:47 +0700

    up pretest01 webapp

--- Pretest01WebApp/Models/TblEmp.cs
+++ Pretest01WebApp/Models/TblEmp.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Pretest01WebApp.Models;
 
 public partial class TblEmp
 {
+    [Required(ErrorMessage = "Please enter EmpId.")]
     public string EmpId { get; set; } = null!;
-
+    [Required(ErrorMessage = "Please enter First Name.")]
     public string? FirstName { get; set; }
-
+    [Required(ErrorMessage = "Please enter Last Name.")]
     public string? LastName { get; set; }
-
+    [Required(ErrorMessage = "Please enter password.")]
     public string? Password { get; set; }
-
+    [Required(ErrorMessage = "Please enter salary.")]
     public double? Salary { get; set; }
 }
